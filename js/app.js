6c5405fae3c7669d960c3dc151b948d32bd16f72
Tushar Agarwal <niftytushar@gmail.com>
2015-01-04 23:56:35 +0530

    set track data

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@
 	var Constants = {
 		SC_Client_Id: '9b798eddaab7753e20d6b7ee62d127e1'
 	};
-	var track = null;
+	var track = {
+		data: null,
+		stream: null
+	};
 
 	init();
 
@@ -11,6 +14,7 @@
 
 		registerEventHandlers();
 
+		getTrackData('116529167', setTrackDetails);
 		streamTrackById('116529167', playTrack);
 	};
 
@@ -23,22 +27,36 @@
 	function registerEventHandlers () {
 		$(".btn-play").on("click", playTrack);
 		$(".btn-pause").on("click", pauseTrack);
-	}
+	};
+
+	function getTrackData (id, callback) {
+		SC.get("/tracks/" + id, function (sound) {
+			track.data = sound;
+			callback();
+		});
+	};
+
+	function setTrackDetails () {
+		$(".song-title").html(track.data.title);
+		$(".song-author span").html(track.data.user.username);
+		$(".song-singer").html("");
+		$(".song-duration").html(track.data.duration);
+	};
 
 	// Stream a track by ID
 	function streamTrackById(id, callback) {
 		SC.stream("/tracks/" + id, function(sound){
-			track = sound;
+			track.stream = sound;
 			callback();
 		});
 	};
 
 	function playTrack() {
-		track.play();
+		track.stream.play();
 	};
 
 	function pauseTrack() {
-		track.pause();
+		track.stream.pause();
 	};
 
 	function toggleButtons () {
