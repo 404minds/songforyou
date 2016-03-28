4ea19bf7179918a61dbeeab525370dc361680070
Tushar Agarwal <niftytushar@gmail.com>
2016-03-28 14:06:18 +0530

    create route

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Create routes
+var routes = require('./routes')(app);
+
 
 // create PORT
 var port = process.env.PORT || 3011;
 var server = app.listen(port, function () {
-	var dPort = server.address().port;
-	console.log("Song 4 u is running on port: ", dPort);
+  var dPort = server.address().port;
+  console.log("SongForYou is running on port: ", dPort);
 });
